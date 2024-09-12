62566d7df5ee9f56adc41ddde0b15861d3d4372d
Arthur Mello Pimentel <Arthurmellopimentel76@gmail.com>
2024-09-12 09:10:38 -0300

    Add files via upload

---
+++ Applivro2/app/src/main/java/com/example/applivro/CarouselAdapter.java
@@ -0,0 +1,44 @@
+package com.example.applivro;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.CarouselViewHolder> {
+
+    private final List<Integer> items;
+
+    public CarouselAdapter(List<Integer> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public CarouselViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.carousel_item, parent, false);
+        return new CarouselViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CarouselViewHolder holder, int position) {
+        holder.imageView.setImageResource(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    static class CarouselViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public CarouselViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //imageView = itemView.findViewById(R.id.carousel_image);
+        }
+    }
+}
