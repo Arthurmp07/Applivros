d65c16dec347e49e06e572b6686520f9a6095f47
Arthurmp07 <Arthurmellopimentel76@gmail.com>
2024-09-19 11:51:35 -0300

    Menu e paginas base

--- app/src/main/java/com/example/livroapp/MainActivity.java
+++ app/src/main/java/com/example/livroapp/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.livroapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +12,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Encontrando as ImageViews
+        ImageView imageView2 = findViewById(R.id.imageView2);
+        ImageView imageView5 = findViewById(R.id.imageView5);
+        ImageView imageView6 = findViewById(R.id.imageView6);
+        ImageView imageView7 = findViewById(R.id.imageView7);
+        ImageView imageView8 = findViewById(R.id.imageView8);
+
+        // Definindo os onClickListeners para redirecionar para diferentes páginas
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Page1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Page2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Page3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Page4Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
