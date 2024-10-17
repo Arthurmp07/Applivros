103cc0984fde599ec8fc2a703a64808f8ee2f6fb
Arthur Mello Pimentel <Arthurmellopimentel76@gmail.com>
2024-10-17 00:52:25 -0300

    Menu retratil na main

--- src/main/java/com/example/livroapp/MainActivity.java
+++ src/main/java/com/example/livroapp/MainActivity.java
@@ -2,36 +2,72 @@ package com.example.livroapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Encontrando as ImageViews
+        // Configuração do Drawer (menu lateral)
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        // Configura o clique na imagem para abrir o menu
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        // Lida com os cliques nas opções do menu utilizando if-else
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.page1) {
+                    startActivity(new Intent(MainActivity.this, Page1Activity.class));
+                } else if (id == R.id.page2) {
+                    startActivity(new Intent(MainActivity.this, Page2Activity.class));
+                } else if (id == R.id.page3) {
+                    startActivity(new Intent(MainActivity.this, Page3Activity.class));
+                } else if (id == R.id.page4) {
+                    startActivity(new Intent(MainActivity.this, Page4Activity.class));
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        // Configuração dos onClickListeners para as ImageViews
         ImageView imageView2 = findViewById(R.id.imageView2);
         ImageView imageView5 = findViewById(R.id.imageView5);
         ImageView imageView6 = findViewById(R.id.imageView6);
         ImageView imageView7 = findViewById(R.id.imageView7);
-        ImageView imageView8 = findViewById(R.id.imageView8);
 
-        // Definindo os onClickListeners para redirecionar para diferentes páginas
-
-        //leva pra personagens
+        // Leva para "Personagens"
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Page1Activity.class);
+                Intent intent = new Intent(MainActivity.this, Page3Activity.class);
                 startActivity(intent);
             }
         });
 
-            // leva pra +info
+        // Leva para "Mais Informações"
         imageView5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,18 +76,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-            //leva pra linha do tempo
+        // Leva para "Linha do Tempo"
         imageView6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Page3Activity.class);
+                Intent intent = new Intent(MainActivity.this, Page1Activity.class);
                 startActivity(intent);
             }
         });
 
-
-//leva pro autor
+        // Leva para "Autor"
         imageView7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,8 +93,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-
     }
 }
--- src/main/java/com/example/livroapp/Page1Activity.java
+++ src/main/java/com/example/livroapp/Page1Activity.java
@@ -12,6 +12,7 @@ public class Page1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_page1);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_page1);
